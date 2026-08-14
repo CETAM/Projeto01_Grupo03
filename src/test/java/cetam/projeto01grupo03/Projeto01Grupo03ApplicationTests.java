5223ec7651a85a845eda699cba190d90bf90d781
Cleverson S. Sobrinho <cleverson.silva@msn.com>
2026-08-14 13:00:19 -0400

    application.propert

--- src/test/java/cetam/projeto01grupo03/Projeto01Grupo03ApplicationTests.java
+++ src/test/java/cetam/projeto01grupo03/Projeto01Grupo03ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Projeto01Grupo03ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads ( ) {
+    }
 
 }
