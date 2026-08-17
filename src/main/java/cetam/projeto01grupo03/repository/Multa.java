aeb5db2ec6985cb2059673bfdfbe5edf3967b33b
Breno0409-Byte <brenobr0409@gmail.com>
2026-08-17 16:39:01 -0400

    Correção de nome: MultaRepository

--- src/main/java/cetam/projeto01grupo03/repository/Multa.java
+++
@@ -1,6 +0,0 @@
-package cetam.projeto01grupo03.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Multa extends JpaRepository <Multa, Long> {
-}
---
+++ src/main/java/cetam/projeto01grupo03/repository/MultaRepository.java
@@ -0,0 +1,7 @@
+package cetam.projeto01grupo03.repository;
+
+import cetam.projeto01grupo03.model.Multa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MultaRepository extends JpaRepository <Multa, Long> {
+}
