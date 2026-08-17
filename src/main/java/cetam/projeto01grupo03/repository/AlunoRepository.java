1c1cef29a4b2b4b369798a74b32bd71a4a6b7b1b
Breno0409-Byte <brenobr0409@gmail.com>
2026-08-17 15:33:28 -0400

    Criação das interfaces do pacote repository

---
+++ src/main/java/cetam/projeto01grupo03/repository/AlunoRepository.java
@@ -0,0 +1,7 @@
+package cetam.projeto01grupo03.repository;
+
+import cetam.projeto01grupo03.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlunoRepository extends JpaRepository <Aluno, Long> {
+}
