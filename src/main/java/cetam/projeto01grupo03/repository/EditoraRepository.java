50876da0e5df6a04bc095c07bc68edacea9bfea3
Wesley Neves <wesleyneves675@gmail.com>
2026-08-18 14:59:44 -0400

    feat(repository): adiciona metodo findByNomeContainingIgnoreCase em EditoraRepository

--- src/main/java/cetam/projeto01grupo03/repository/EditoraRepository.java
+++ src/main/java/cetam/projeto01grupo03/repository/EditoraRepository.java
@@ -3,5 +3,8 @@ package cetam.projeto01grupo03.repository;
 import cetam.projeto01grupo03.model.Editora;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EditoraRepository extends JpaRepository<Editora, Long> {
+    List<Editora> findByNomeContainingIgnoreCase(String nome);
 }
