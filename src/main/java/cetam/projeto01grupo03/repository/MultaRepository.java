46e3de2d172900ca1af3de5ab1bd49543d0a44d2
Wesley Neves <wesleyneves675@gmail.com>
2026-08-19 16:00:30 -0400

    atualização da etidade livro e repositoy multa

--- src/main/java/cetam/projeto01grupo03/repository/MultaRepository.java
+++ src/main/java/cetam/projeto01grupo03/repository/MultaRepository.java
@@ -1,7 +1,15 @@
 package cetam.projeto01grupo03.repository;
 
 import cetam.projeto01grupo03.model.Multa;
+import cetam.projeto01grupo03.model.StatusMulta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MultaRepository extends JpaRepository <Multa, Long> {
+    List<Multa> findByStatus(StatusMulta status);
+
+    List<Multa> findByEmprestimoAlunoId(Long alunoId);
+
+    List<Multa> findByEmprestimoAlunoNomeContainingIgnoreCaseOrEmprestimoAlunoMatriculaContainingIgnoreCase(String nome, String matricula);
 }
