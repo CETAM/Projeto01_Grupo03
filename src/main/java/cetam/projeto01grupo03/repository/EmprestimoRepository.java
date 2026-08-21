6355cd9d084bde5993bc1808bec38a2a4b92fee6
Wesley Neves <wesleyneves675@gmail.com>
2026-08-21 14:32:24 -0400

    feat(respository):ajustes nos repositories Emprestimo, Livro e Multa

--- src/main/java/cetam/projeto01grupo03/repository/EmprestimoRepository.java
+++ src/main/java/cetam/projeto01grupo03/repository/EmprestimoRepository.java
@@ -1,9 +1,12 @@
 package cetam.projeto01grupo03.repository;
 
+import cetam.projeto01grupo03.dto.LivroMaisEmprestadoDTO;
 import cetam.projeto01grupo03.model.Emprestimo;
 import cetam.projeto01grupo03.model.StatusEmprestimo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.awt.print.Pageable;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -19,5 +22,25 @@ public interface EmprestimoRepository extends JpaRepository <Emprestimo, Long> {
     List<Emprestimo> findByLivroId(Long livroId);
 
     List<Emprestimo> findByStatusAndDataPrevisaoDevolucaoBefore(StatusEmprestimo status, LocalDate data);
+
+    List<Emprestimo> findByDataEmprestimoBetween(LocalDate inicio, LocalDate fim);
+
+    List<Emprestimo> findByDataEmprestimoBetweenAndStatus(LocalDate inicio, LocalDate fim, StatusEmprestimo status);
+
+    long countByStatus(StatusEmprestimo status);
+
+    long countByDataEmprestimo(LocalDate data);
+
+    long countByDataDevolucao(LocalDate data);
+
+    long countByDataPrevisaoDevolucaoAndStatusNot(LocalDate data, StatusEmprestimo status);
+
+    List<Emprestimo> findAllByOrderByIdDesc(Pageable pageable);
+
+    @Query("SELECT new br.com.biblioteca.biblioteca.dto.LivroMaisEmprestadoDTO(e.livro.titulo, e.livro.autor.nome, e.livro.categoria.nome, COUNT(e)) " +
+            "FROM Emprestimo e " +
+            "GROUP BY e.livro.id, e.livro.titulo, e.livro.autor.nome, e.livro.categoria.nome " +
+            "ORDER BY COUNT(e) DESC")
+    List<LivroMaisEmprestadoDTO> findLivrosMaisEmprestados(Pageable pageable);
 
 }
